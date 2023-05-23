762bcb98f0d5a48a570b93974aea7cba9078b127
zurajeladze2 <zurajeladze2@gmail.com>
2023-05-23 08:34:00 -0700

    empty

--- 11 - jquery1/search bar/main.js
+++
@@ -1,6 +0,0 @@
-$('.logo').click(function () { 
-        $('.logo').hide();
-        $('.helo').animate({height: '900vw',width: '900vw',},
-        1000);
-        $('#search-bar').show()
-});
--- 11 - jquery1/slides/main.js
+++
@@ -1,9 +0,0 @@
-$('#up').click(function (e) { 
-    $('.yellow-div').slideUp();
-});
-$('#down').click(function (e) { 
-    $('.yellow-div').slideDown();
-});
-$('#toggle').click(function (e) { 
-    $('.yellow-div').slideToggle();
-});
