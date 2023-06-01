a9da094306a1e9f4a92933f6a6cd6778d451d190
zurajeladze2 <Zurajeladze2@gmail.com>
2023-06-02 03:05:34 +0400

    FinalProjects daemata ProductComponent

--- Final Project/finalProject/src/app/components/product/product.component.ts
+++ Final Project/finalProject/src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { HTTPModuleService } from 'src/app/services/httpmodule.service';
 
 @Component({
   selector: 'app-product',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+  constructor(private myService: HTTPModuleService, private route: ActivatedRoute){}
+
+  productId: any;
+  productsArray:any;
+  
+  ngOnInit(){
+    this.getProducts()
+  }
+  
+  getProducts(){
+    this.myService.getAllProducts().subscribe(async (res) => {
+      for (const key in res) {
+        if (Object.prototype.hasOwnProperty.call(res, key)) {
+          const element = res[key];
+          this.productsArray = element;
+          this.productId = this.route.snapshot.paramMap.get('id');
+          console.log("productId is "+this.productId);
+          console.warn('array:');
+          console.log(this.productsArray);
+          
+          
+          
+        }
+      }
+    })
+  }
 }
